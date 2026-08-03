4dfe1a26d08372a4f9317da9c3f52b8ec47a5d7f
abdalaziz babker <abdalazizlaptop@gmail.com>
2026-08-03 22:48:01 +0300

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        navigator.serviceWorker.register('./sw.js', { scope: './' }).then(function(registration) {
             console.log('ServiceWorker registered with scope:', registration.scope);
         }).catch(function(error) {
             console.log('ServiceWorker registration failed:', error);
